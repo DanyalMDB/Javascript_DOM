3cd58adc36b3feb62571db3c2f15ea1e46b00fb1
Muhammad Butt <muhammad.butt@stud.th-deg.de>
2020-09-11 02:02:37 +0200

    looking at different events and how to attach them to DOM elements with addEventListener();

--- dom.js
+++ dom.js
@@ -96,16 +96,202 @@
 
 
 
-//QuerySelectorAll
-var title = document.querySelectorAll('.title')
+// //QuerySelectorAll
+// var title = document.querySelectorAll('.title')
 
-console.log(title[0].textContent='Neeehaaa');
+// console.log(title[0].textContent='Neeehaaa');
 
-var odd= document.querySelectorAll('li:nth-child(odd)')
-var even= document.querySelectorAll('li:nth-child(even)')
+// var odd= document.querySelectorAll('li:nth-child(odd)')
+// var even= document.querySelectorAll('li:nth-child(even)')
 
-for (let i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor='purple';  
-    even[i].style.backgroundColor='#ccc';  
+// for (let i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor='purple';  
+//     even[i].style.backgroundColor='#ccc';  
 
+// }
+
+ //TRAVERSING THE DOM   
+
+ var itemList = document.querySelector('#items')
+
+// //parentNode
+// console.log(itemList.parentNode);
+
+// itemList.parentNode.style.backgroundColor='yellow'
+
+// console.log(itemList.parentNode.parentNode)
+// itemList.parentNode.parentNode.style.backgroundColor='blue'
+
+// //parentElement
+// console.log(itemList.parentElement);
+
+// itemList.parentElement.style.backgroundColor='yellow'
+
+// console.log(itemList.parentElement.parentElement)
+// itemList.parentElement.parentElement.style.backgroundColor='black'
+
+
+// //childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+
+// console.log(itemList.children[1]);
+
+// itemList.children[1].style.backgroundColor='#ccc'
+
+// //firstelementchild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello1'
+
+
+// //Lastelementchild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='Hello4'
+
+
+// //NextSibling
+
+// console.log(itemList.nextSibling) //gve us text space
+
+// //nextElementSibling
+// console.log(itemList.nextElementSibling)
+
+
+// // //PreviousSibling
+
+// // console.log(itemList.previousSibling)
+
+// // //nextElementSibling
+// console.log(itemList.previousElementSibling)
+
+// itemList.previousElementSibling.style.color='green'
+
+
+//Now creating Elements
+
+//create a div
+
+// var newDiv=document.createElement('div')
+
+// //Add Class
+// newDiv.className='hello'
+// //Add Id
+// newDiv.id='idhello'
+
+// //Add attribute
+// newDiv.setAttribute('title','Title bro  ')
+
+// //Create text node
+// var newDivText = document.createTextNode('New Wolrrld')
+
+// //Add text to div
+// newDiv.appendChild(newDivText)
+// console.log(newDiv);
+// //puting in html
+// var container=document.querySelector('header .container')
+// var h1= document.querySelector('header h1')
+
+// console.log(newDiv)
+// newDiv.style.fontSize='55px'
+// container.insertBefore(newDiv,h1);
+ 
+// //OLD Method 
+
+// function buttonClick1(){
+//     console.log('Button clicked');
+// }
+
+// var button = document.getElementById('button').addEventListener(
+//     'click',function(){
+//         console.log(123);
+//     })
+
+
+
+
+// EVENTS
+
+// var button = document.getElementById('button').addEventListener(
+//     'click',buttonClick1)
+// function buttonClick1(e){
+    // // console.log('Button clicked');
+    // document.getElementById('header-title').textContent='Changed'
+    // document.querySelector('#main').style.backgroundColor='#f4f4f4'
+
+//     console.log(e.target);
+//     console.log(e.target.id);
+//     console.log(e.target.className);
+
+//     var output= document.getElementById('output')
+//    output.innerHTML='<h3>'+e.target.id+'<h3>' 
+
+// //    console.log(e.type);
+
+// console.log(e.clientX);//from screen 
+// console.log(e.clientY);
+
+// console.log(e.offsetX);//from the button
+// console.log(e.offsetY);//from the button
+
+// console.log(e.altKey); //alt+click
+// console.log(e.ctrlKey);
+// console.log(e.shiftKey);
+
+// var button = document.getElementById('button').addEventListener(
+//     'click',runEvent)//single click
+    // var button = document.getElementById('button').addEventListener(
+    //     'dblclick',runEvent)//double click
+    // var button = document.getElementById('button').addEventListener(
+    //     'mousedown',runEvent)//mouse down click
+        // var button = document.getElementById('button').addEventListener(
+        //     'mouseup',runEvent)//mouse up click
+
+    // var button = document.getElementById('button')
+//     var box= document.getElementById('box')
+
+//    // box.addEventListener('mouseenter',runEvent)
+//     //box.addEventListener('mouseleave',runEvent)
+
+// //     box.addEventListener('mouseover',runEvent)
+// //    // box.addEventListener('mouseout',runEvent)
+//    box.addEventListener('mousemove',runEvent)
+
+var itemInput = document.querySelector('input[type="text"]')
+var form= document.querySelector('form')
+
+var select= document.querySelector('select')
+
+
+// itemInput.addEventListener('keydown',runEvent)
+// itemInput.addEventListener('keyup',runEvent)
+// itemInput.addEventListener('keypress',runEvent)
+
+// itemInput.addEventListener('blur',runEvent)
+// itemInput.addEventListener('focus',runEvent)
+
+
+// itemInput.addEventListener('cut',runEvent)
+// itemInput.addEventListener('paste',runEvent)
+
+// select.addEventListener('change',runEvent)
+
+    //     function runEvent(event) {
+    // console.log('Event Type'+event.type)}
+
+    form.addEventListener('submit',runEvent)
+
+
+    function runEvent(e) {
+    e.preventDefault()
+    // output.innerHTML='<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>'
+
+    // box.style.backgroundColor="rgb("+e.offsetX+","+e.offsetY+",40)"
+    
+    console.log('Event Type'+e.type);
+    // console.log('Value'+e.target.value);
+   // console.log(e.target.value);
+    // document.getElementById('output').innerHTML='<h3>'
+    // +e.target.value+'</h3>'
 }
+
